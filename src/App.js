99a1342861b67a6699041c2820418db7b844bc52
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-24 15:46:35 -0600

    estructura inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import pokeball from './assets/pokeball.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-nav'>
+        <img className='pokemon-icon' src={pokeball} alt='icon' />
+        <p className='title'>Pokedex</p>
+      </div>
     </div>
   );
 }
