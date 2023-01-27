33400713d3d993456a6cbdf090d3a98b4cf9e847
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-26 19:22:29 -0600

    mejoras en la aparencia de galeria

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "./Modal.js";
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf,faFire, faWater,faIcicles,faBolt, faFlask} from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from "react";
 
 const PokemonsList = () => {
@@ -9,6 +11,14 @@ const PokemonsList = () => {
 	const [modalIsOpen, setModalIsOpen] = useState(false)
 	const [selectedPokemon, setSelectedPokemon] = useState({})
 
+	const typeIcons = {
+		"grass": <FontAwesomeIcon icon={faLeaf}/>,
+		"fire": <FontAwesomeIcon  icon={faFire}/>,
+		"water": <FontAwesomeIcon icon={faWater}/>,
+		"ice": <FontAwesomeIcon icon={faIcicles}/>,
+		"electric": <FontAwesomeIcon icon={faBolt}/>,
+		"poison": <FontAwesomeIcon icon={faFlask}/>
+	}
 
 	useEffect(() => {
 		axios
@@ -37,21 +47,29 @@ const PokemonsList = () => {
 	}
 
 	return (
-		<div>
-			<div>
 				<div className="container-grid">
 				{pokemonDetails.map((pokemonDetail) => (
-					<div key={pokemonDetail.name} onClick={() => handleClick(pokemonDetail)} >
-						<h2 className="grid-item">{pokemonDetail.name}</h2>
-                        <img className="grid-item" src={pokemonDetail.sprites.front_default} alt="pokemon"/>
+					<div className="list-item" key={pokemonDetail.name} onClick={() => handleClick(pokemonDetail)}>
+						<div className="id-item">{`#${pokemonDetail.id.toString().padStart(3,"0")}`}</div>
+                        <img className="img-item" src={pokemonDetail.sprites.front_default} alt="pokemon"/>
+						<h2 className="text-item">{pokemonDetail.name}</h2>
+						<div className="types">
+							{pokemonDetail.types.map((types, i) => {
+								if(typeIcons[types.type.name]){
+									return <p className={types.type.name} key={i}>{typeIcons[types.type.name]}</p>
+								} else{
+									return <p key={i}>Tipo desconocido</p>
+
+								}
+							})}
+						</div>
+			
 					</div>
 				))}
-			</div>
 			{ modalIsOpen &&
 				<Modal pokemon={selectedPokemon} closeModal={() => setModalIsOpen(false)} />
 			}
 			</div>
-		</div>
 	);
 };
 
